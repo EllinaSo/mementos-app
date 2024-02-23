1a36dc591008ca87f34d043f7b0db3f08094f875
Ellina S <ellina.ibr@gmail.com>
2024-02-23 17:03:35 -0600

    Updated backend using eslint

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,69 +1,68 @@
-
-import mongoose from "mongoose";
-import PostMessage from "../models/postMessage.js";
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find().sort('-createdAt');
-        res.status(200).json(postMessages);
-    } catch ({ message }) {
-        res.status(404).json({ message });
-    }
-}
+  try {
+    const postMessages = await PostMessage.find().sort('-createdAt');
+    res.status(200).json(postMessages);
+  } catch ({ message }) {
+    res.status(404).json({ message });
+  }
+};
 
 export const createPost = async (req, res) => {
-    const newPost = new PostMessage(req.body);
-    try {
-        await newPost.save();
-        res.status(201).json(newPost);
-    } catch ({ message }) {
-        res.status(409).json({ message });
-    }
-}
+  const newPost = new PostMessage(req.body);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch ({ message }) {
+    res.status(409).json({ message });
+  }
+};
 
-const NO_POST_MESSAGE = "No post with that id"
+const NO_POST_MESSAGE = 'No post with that id';
 
 export const updatePost = async (req, res) => {
-    const { id } = req.params;
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send(NO_POST_MESSAGE);
-    }
+  const { id } = req.params;
 
-    const post = req.body;
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new : true });
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(NO_POST_MESSAGE);
+  }
 
-    if (updatedPost) {
-        res.status(200).json(updatedPost);
-    } else {
-        return res.status(404).send(NO_POST_MESSAGE);
-    }
-}
+  const post = req.body;
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
+  if (updatedPost) {
+    res.status(200).json(updatedPost);
+  } else {
+    return res.status(404).send(NO_POST_MESSAGE);
+  }
+};
 
 export const likePost = async (req, res) => {
-    const { id } = req.params;
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send(NO_POST_MESSAGE);
-    }
+  const { id } = req.params;
 
-    const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount : post.likeCount + 1}, { new : true });
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(NO_POST_MESSAGE);
+  }
 
-    if (updatedPost) {
-        res.status(200).json(updatedPost);
-    } else {
-        return res.status(404).send(NO_POST_MESSAGE);
-    }
-}
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+  if (updatedPost) {
+    res.status(200).json(updatedPost);
+  } else {
+    return res.status(404).send(NO_POST_MESSAGE);
+  }
+};
 
 export const deletePost = async (req, res) => {
-    const { id } = req.params;
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send(NO_POST_MESSAGE);
-    }
+  const { id } = req.params;
 
-    await PostMessage.findByIdAndDelete(id);
-    res.status(200).send('Post deleted successfully');
-}
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send(NO_POST_MESSAGE);
+  }
+
+  await PostMessage.findByIdAndDelete(id);
+  res.status(200).send('Post deleted successfully');
+};
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js'
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 const routes = express.Router();
 
-routes.get('/', getPosts)
+routes.get('/', getPosts);
 routes.post('/', createPost);
 routes.patch('/:id', updatePost);
 routes.patch('/:id/like', likePost);
